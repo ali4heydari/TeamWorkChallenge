74df9b7e9896649d68739c28cab744e8545e3be0
Ali Heydari <ali4heydari@gmail.com>
2018-10-25 13:47:45 +0330

    Improvement

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,7 +1,12 @@
 package models;
 
 public class Player {
-    public void play() {
+    private City city;
+    public static final int INITIAL_LEVEL = 1;
+    public static final int INITIAL_ID = 1;
 
+
+    public void play() {
+        this.city = new City(INITIAL_LEVEL, INITIAL_ID);
     }
 }
